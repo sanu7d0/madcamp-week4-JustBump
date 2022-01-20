3234f8e85b871f421d6075ec27acf79177574297
Sanu7D0 <83103105+Sanu7D0@users.noreply.github.com>
2022-01-20 14:40:13 +0900

    Add game rule

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -5,19 +5,60 @@ using UnityEngine;
 sealed public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private int _gameGoalScore;
+        public int gameGoalScore {
+            get { return _gameGoalScore; }
+        }
+    [SerializeField] private int _gameLimitTime;
+        public int gameLimitTime {
+            get { return _gameLimitTime; }
+        }
+    public float gameStartTime;
+    public float gameElapsedTime {
+        get { return Time.time - gameStartTime; }
+    }
 
-    protected override void Awake()
-    {
+    private bool _isPlaying;
+        public bool isPlaying {
+            get { return _isPlaying; }
+        }
+
+    protected override void Awake() {
         base.Awake();
     }
 
     void Start() {
+        StartGame();
+    }
+
+    private void StartGame() {
+        _isPlaying = true;
+        
         GameObject player = Instantiate(playerPrefab, GameObject.Find("Spawn0").transform.position, Quaternion.identity);
         AttachMainCamera(player);
+        
+        gameStartTime = Time.time;
+        StartCoroutine(StartTimer());
+
+        Debug.Log("Game started");
+    }
+
+    private void EndGame() {
+        _isPlaying = false;
+        Debug.Log("Game ended");
     }
 
     private void AttachMainCamera(GameObject target) {
         GameObject camera = GameObject.Find("Main Camera");
         camera.GetComponent<CameraController>().targetTransform = target.transform;
+    }
+
+    IEnumerator StartTimer() {
+        while (gameElapsedTime < gameLimitTime) {
+            Debug.Log("Time elapsed for " + gameElapsedTime);
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Time limit reached!");
+        EndGame();
     }
 }
---
+++ Assets/Scripts/Helpers/SingletonP.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Photon.Pun;
+
+// This is a singleton for photon mono behaviour
+public class SingletonP<T> : MonoBehaviourPun where T : Component
+{
+    private static T _instance;
+    public static T Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null) {
+                    GameObject newGO = new GameObject();
+                    _instance = newGO.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake() {
+        _instance = this as T;
+    }
+}
