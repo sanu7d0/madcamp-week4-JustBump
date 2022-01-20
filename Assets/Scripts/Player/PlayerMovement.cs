c045cad7240cecb857882b71619fd090cef1409c
Sanu7D0 <83103105+Sanu7D0@users.noreply.github.com>
2022-01-21 02:11:08 +0900

    Change roll logic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,142 +1,123 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private PlayerController playerController;
     private Vector2 lastMoveDir;
-    private SpriteRenderer spriteRenderer;
-    private Transform transform;
-    private int point_num = 20;
     Animator anim;
-    private Vector2[] waypoints;
-    private int wayPointIndex = 0;
-    [SerializeField] public float jumpPower;
-    [SerializeField] private float rollingSpeed;
     [SerializeField] private float speed;
+    [SerializeField] private float rollingSpeed;
+    [SerializeField] private float rollingTime;
+
+    private State state;
+    private enum State {
+        Normal,
+        Rolling
+    }
 
     void Awake() {
         rb = GetComponent<Rigidbody2D>();
         playerController = GetComponent<PlayerController>();
         anim = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        transform = GetComponent<Transform>();
-        waypoints = null;
-        wayPointIndex = 0;
+        state = State.Normal;
     }
     
     void Start() {
-        playerController.onJump.AddListener(jump);
-        playerController.onRoll.AddListener(roll);
-
+        playerController.onRoll.AddListener(HandleRoll);
     }
 
     void Update()
     {
-        HandleMovement();
-        HandleRolling();
-    }
-
-    /*void FixedUpdate() {
-        HandleMovement();
-    }*/
-
-    private void HandleRolling() {
-        if (waypoints == null) return;
-        if (wayPointIndex == point_num) {
-            wayPointIndex = 0;
-            waypoints = null;
-            anim.SetBool("isRolling", false);
-        }
-        Vector2 currPos = transform.position;
-
-        
-        transform.position = Vector2.MoveTowards(currPos, waypoints[wayPointIndex++], 5 * Time.deltaTime);
-    }
-
-    private void jump() {
-        Debug.Log("Jump!!!");
-        rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-    }
-
-    private void roll() {
-        Debug.Log("Roll@@@");
-        anim.SetBool("isRolling", true);
-        
-        waypoints = new Vector2[point_num];
-        
-        wayPointIndex = 0;
-        for (int i = 0; i < point_num; i++) {
-            Vector2 moveDir = playerController.moveDir;
-            waypoints.SetValue(moveDir * i, i);
+        switch (state) {
+            case State.Normal:
+                HandleMovement();
+                break;
+            
+            case State.Rolling:
+                HandleRollSliding();
+                break;
         }
     }
-
-    /*private void HandleMovement() {
-        Vector2 moveDir = playerController.moveDir;
-        lastMoveDir = moveDir;
-
-        // rb.AddForce(moveDir * speed * Time.deltaTime); // option 1
-        rb.velocity = moveDir * speed * Time.deltaTime; // option 2
-
-        if (rb.velocity.x == 0 && rb.velocity.y == 0) {
-            anim.SetBool("isWalking", false);
-        } else {
-            anim.SetBool("isWalking", true);
-        }
-
-        // spriteRenderer.flipX = true;
-        if (moveDir.x < 0) {
-            transform.SetPositionAndRotation(transform.position,
-            Quaternion.Euler(0f, 180f, 0f));
-        } else if (moveDir.x > 0) {
-            transform.SetPositionAndRotation(transform.position,
-            Quaternion.Euler(0f, 0f, 0f));
-        }
-
-        // if (moveDir.)
-
-        // rb.MovePosition(rb.position + (moveDir * speed * Time.deltaTime));  // option 3
-        // * MovePosition -> friction을 무시한다
-    }*/
     
-    // Movement option 4
     private void HandleMovement() {
-        Vector3 moveDir = playerController.moveDir;
-        Vector3 baseMoveDir = moveDir;
+        Vector2 moveDir = playerController.moveDir;
         lastMoveDir = moveDir;
 
         bool isIdle = moveDir.x == 0 && moveDir.y == 0;
         if (isIdle) {
             anim.SetBool("isWalking", false);
-            // 여기에 idle 애니메이션 call 추가
-            // ex) animator.playIdleAnimation(lastMovedir);
         } else {
-            bool canMove = CanMove(moveDir, speed * Time.deltaTime);
-            if (!canMove) {
-                // Cannot move diagonally
-                moveDir = new Vector3(moveDir.x, 0f).normalized;
-                canMove = moveDir.x != 0f && CanMove(moveDir, speed * Time.deltaTime);
-
-                if (!canMove) {
-                    // Cannot move horizontally
-                    moveDir = new Vector3(0f, baseMoveDir.y).normalized;
-                    canMove = moveDir.y != 0f && CanMove(moveDir, speed * Time.deltaTime);
-                }
-            }
-
-            if (canMove) {
-                lastMoveDir = moveDir;
-                // playWalkingAnimation(moveDir);
-                transform.position += moveDir * speed * Time.deltaTime;
+            if (TryMove(moveDir, speed * Time.deltaTime)) {
+                anim.SetBool("isWalking", true);
             } else {
-                // playIdleAnimation(lastMoveDir);
+                anim.SetBool("isWalking", false);
             }
         }
+
+        // Flip transform
+        if (lastMoveDir.x < 0) {
+            transform.SetPositionAndRotation(transform.position,
+            Quaternion.Euler(0f, 180f, 0f));
+        } else if (lastMoveDir.x > 0) {
+            transform.SetPositionAndRotation(transform.position,
+            Quaternion.Euler(0f, 0f, 0f));
+        }
+    }
+
+    private void HandleRoll() {
+        // Cannot roll while not moving or rolling
+        if (lastMoveDir == Vector2.zero || state == State.Rolling) {
+            return;
+        }
+
+        // Start roll
+        StartCoroutine(RollRoutine());
+    }
+
+    IEnumerator RollRoutine() {
+        state = State.Rolling;
+        anim.SetBool("isRolling", true);
+        Debug.Log("Roll start");
+
+        yield return new WaitForSeconds(rollingTime);
+
+        state = State.Normal;
+        anim.SetBool("isRolling", false);
+        Debug.Log("Roll end");
+    }
+
+    private void HandleRollSliding() {
+        TryMove(lastMoveDir, rollingSpeed * Time.deltaTime);
     }
 
     private bool CanMove(Vector3 dir, float distance) {
         return Physics2D.Raycast(transform.position, dir, distance).collider == null;
+    }
+
+    private bool TryMove(Vector3 baseMoveDir, float distance) {
+        Vector3 moveDir = baseMoveDir;
+        bool canMove = CanMove(moveDir, distance);
+
+        if (!canMove) {
+            // Cannot move diagonally
+            moveDir = new Vector3(moveDir.x, 0f).normalized;
+            canMove = moveDir.x != 0f && CanMove(moveDir, distance);
+
+            if (!canMove) {
+                // Cannot move horizontally
+                moveDir = new Vector3(0f, baseMoveDir.y).normalized;
+                canMove = moveDir.y != 0f && CanMove(moveDir, distance);
+            }
+        }
+
+        if (canMove) {
+            lastMoveDir = moveDir;
+            transform.position += moveDir * distance;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
